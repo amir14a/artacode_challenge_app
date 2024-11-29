21fb8b3a8b941e51c547af3b69981f334577d523
AmirAbbas-PC <a.abbasj@yahoo.com>
2024-11-29 15:51:37 +0330

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# artacode_challenge_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/artacode_challenge_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.artacode_challenge_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
